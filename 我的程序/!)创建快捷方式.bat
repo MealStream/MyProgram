83478589b965cd33183c5084377a140acf1d248a
LeftRight <75202334+MealStream@users.noreply.github.com>
2022-07-22 01:27:15 +0800

    Update !)创建快捷方式.bat

--- 我的程序/!)创建快捷方式.bat
+++ 我的程序/!)创建快捷方式.bat
@@ -4,5 +4,4 @@ powershell -Command "Start-Process '%~sdpnx0' -Verb RunAs"&&EXIT)
 ::创建快捷方式
 mshta VBScript:Execute("Set a=CreateObject(""WScript.Shell""):Set b=a.CreateShortcut(a.SpecialFolders(""Desktop"") & ""\我的程序.lnk""):b.TargetPath=""%~sdp0MyProgram.exe"":b.WorkingDirectory=""%~sdp0"":b.Save:close")
 
-ECHO.&ECHO LeftRight
-ECHO.&ECHO 创建完成 &TIMEOUT /t 2 >NUL&EXIT
+ECHO.&ECHO 完成 &TIMEOUT /t 2 >NUL&EXIT
