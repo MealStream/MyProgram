307c8b691d0230169e9c3658af895e87d7672704
LeftRight <75202334+MealStream@users.noreply.github.com>
2022-07-22 01:14:27 +0800

    Rename 删除.bat to !)删除快捷方式.bat

---
+++ 我的程序/!)删除快捷方式.bat
@@ -0,0 +1,9 @@
+@ECHO OFF&(PUSHD "%~DP0")&(REG QUERY "HKU\S-1-5-19">NUL 2>&1)||(
+powershell -Command "Start-Process '%~sdpnx0' -Verb RunAs"&&EXIT)
+
+::删除快捷方式
+del /q "%Public%\Desktop\我的程序.lnk" >NUL 2>NUL
+del /q "%UserProfile%\Desktop\我的程序.lnk" >NUL 2>NUL
+
+ECHO.&ECHO LeftRight
+ECHO.&ECHO 删除完成 &TIMEOUT /t 2 >NUL&EXIT
