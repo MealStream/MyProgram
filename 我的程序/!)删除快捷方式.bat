43d729309f7d1689ab3b2323ae52226ba2b7630c
LeftRight <75202334+MealStream@users.noreply.github.com>
2022-07-23 02:22:50 +0800

    Add files via upload

--- 我的程序/!)删除快捷方式.bat
+++ 我的程序/!)删除快捷方式.bat
@@ -1,8 +1,8 @@
 @ECHO OFF&(PUSHD "%~DP0")&(REG QUERY "HKU\S-1-5-19">NUL 2>&1)||(
 powershell -Command "Start-Process '%~sdpnx0' -Verb RunAs"&&EXIT)
 
-::鍒犻櫎蹇嵎鏂瑰紡
-del /q "%Public%\Desktop\鎴戠殑绋嬪簭.lnk" >NUL 2>NUL
-del /q "%UserProfile%\Desktop\鎴戠殑绋嬪簭.lnk" >NUL 2>NUL
+::删除快捷方式
+del /q "%Public%\Desktop\我的程序.lnk" >NUL 2>NUL
+del /q "%UserProfile%\Desktop\我的程序.lnk" >NUL 2>NUL
 
-ECHO.&ECHO 瀹屾垚 &TIMEOUT /t 2 >NUL&EXIT
+ECHO.&ECHO 完成 &TIMEOUT /t 2 >NUL&EXIT
